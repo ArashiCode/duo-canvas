65bb797ea25f0b1da784289c26b6b66b7e3834d2
Felix-403 <95995476+Felix-403@users.noreply.github.com>
2021-12-19 11:39:03 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Canvas = require("canvas");
 // Register font
 Canvas.registerFont(`${__dirname}/assets/font/RFlexBold-VGzLZ.ttf`, { family: "RFlexBold" });
+Canvas.registerFont(`${__dirname}/assets/font/Evogria.ttf`, { family: "Evogria" });
 
 /*Module*/
 module.exports.Welcome = require('./src/greetings/Welcome');
 module.exports.Goodbye = require('./src/greetings/Goodbye');
-module.exports.Rank = require('./src/greetings/Rank');
+module.exports.Rank = require('./src/rank/Rank');
 
 
 
