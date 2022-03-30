298b5550e43b862e0c65d4829ab88090226ee6dd
Texas <79846517+Lingz-ui@users.noreply.github.com>
2022-03-30 11:51:08 +0700

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Canvas = require("canvas");
 // Register font
 Canvas.registerFont(`${__dirname}/assets/font/RFlexBold-VGzLZ.ttf`, { family: "RFlexBold" });
 Canvas.registerFont(`${__dirname}/assets/font/Evogria.ttf`, { family: "Evogria" });
+Canvas.registerFont(`${__dirname}/assets/font/novamono.ttf`, { family: "NVM" });
+Canvas.registerFont(`${__dirname}/assets/font/Heavitas.ttf`, { family: "HVitas" });
 
 /*Module*/
 module.exports.Welcome = require('./src/greetings/Welcome');
